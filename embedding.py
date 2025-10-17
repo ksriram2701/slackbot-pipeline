fb879cbccec53437aeae8b25c1cbd048596830e9
Karthik Sriram B A <124027211+ksriram2701@users.noreply.github.com>
2025-10-18 04:02:51 +0530

    Update embedding.py

--- embedding.py
+++ embedding.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
-cluster_model = SentenceTransformer(r"e5-large-v2")
+cluster_model = SentenceTransformer(r"intfloat/e5-large-v2")
 def get_embeddings(keywords):
     return cluster_model.encode(keywords, normalize_embeddings=True)
+
 
